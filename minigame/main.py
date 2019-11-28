54a4f373a9be65993f61a89f6effbfd07ed11a9f
Bakukun <h1101201@gmail.com>
2019-11-28 23:43:22 +0900

    돈을 게임을 재 실행할 때마다 뜨도록 코드 변경 및 게임 변경
    but 다른 게임에서 쓰는 재화가 저장은 잘되는데 main 함수로 다시돌아오면 값이 원래 값으로 초기화가 되는 심각한 오류 발생. 해결해야 겠음

--- minigame/main.py
+++ minigame/main.py
@@ -10,27 +10,30 @@ def main():
     for i in ment:
         time.sleep(0.1)
         print(i, end='')
-    if (money >= 0):
-        print("현재 ", money, "원을 보유하고 계십니다")
-    elif (money < 0):
-        print("현재 ", money, "원을 보유하고 계십니다\n어서 빚을 갚아봐요!")
     ment2 = "로딩중 입니다...\n"
     for k in ment2:
         time.sleep(0.3)
         print(k, end='')
-    main2(money, ID, passwd)
+    while (main2(money, ID, passwd) != 0):
+        members = load_members() #여기서 money를 새로운 값으로 갱신해야 할텐데 모르겠음
 
 
 def main2(money, ID, passwd):
+    if (money >= 0):
+        print("현재 ", money, "원을 보유하고 계십니다")
+    elif (money < 0):
+        print("현재 ", money, "원을 보유하고 계십니다\n어서 빚을 갚아봐요!")
+    time.sleep(0.5)
     print("================================")
     print("1. 게임 설명듣기")
     print("2. 로또")
     print("3. 러시안룰렛")
     print("4. 다함께 암산왕")
-    print("5. 구애의 춤")
-    print("6. 나가기")
+    print("5. 도둑 잡기")
+    print("6. 베스킨라빈스 31")
+    print("7. 나가기")
     number = input("플레이 하실 게임의 번호를 선택해주세요\n")
-    while (number != "1" and number != "2" and number != "3" and number != "4" and number != "5" and number != "6"):
+    while (number != "1" and number != "2" and number != "3" and number != "4" and number != "5" and number != "6" and number != "7"):
         number = input("숫자를 정확하게 입력해주세요!\n")
     if (number == "1"):
         explain()
@@ -44,6 +47,10 @@ def main2(money, ID, passwd):
         print("================================")
     elif (number == "6"):
         print("================================")
+    elif (number == "7"):
+        print("================================")
+        print("게임을 종료합니다 또 만나요~")
+        return 0
 
 
 
