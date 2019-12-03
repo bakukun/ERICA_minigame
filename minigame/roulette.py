3f2bfff58ef6789548411999d816878abbea76d0
Bakukun <h1101201@gmail.com>
2019-12-04 01:33:13 +0900

    재실행시 오류가 나지 않도록 코드변경
    cnt값 초기화가 아닌, index값 설정 오류였음. 바로잡음

--- minigame/roulette.py
+++ minigame/roulette.py
@@ -12,6 +12,7 @@ def roulettegame(money, ID, passwd):
     print("룰은 간단합니다! 세번의 방아쇠를 자신에게 먼저 당겨 총알이 전부 가짜라면 내기 금액의 2배!")
     print("하지만, 그곳에 진짜 총알이 없다면... 내기 금액을 모두 잃습니다")
     print("총알은 총 6개이고, 진짜 총알은 단 한개 입니다")
+    print("보유금액보다 더 많은 금액을 배팅할 시 게임이 거부됩니다.")
     print("게임 구매 비용은 십만원 입니다")
     print("============================\n")
 
@@ -19,7 +20,8 @@ def roulettegame(money, ID, passwd):
     while (start != "Y" and start != "y" and start !=  "N" and  start != "n"):
         start = input("대문자 혹은 소문자로 y와 n을 정확하게 입력해주세요!\n")
     if (start == "Y" or start == "y"):
-        bat = int(input("내기로 거실 금액을 입력해주세요 이기면 두배 지면 전부 잃음!"))
+        print("현재 ",money,"원 보유중입니다")
+        bat = int(input("내기로 거실 금액을 입력해주세요 이기면 두배 지면 전부 잃음!\n"))
         if (money>=bat):
             money -= bat
             print(bat,"원이 차감되어 ",money,"원을 갖고 있습니다.")
@@ -27,7 +29,7 @@ def roulettegame(money, ID, passwd):
             store_members(members)
             roulette_main(money, ID, passwd, bat)
         else:
-            print("당신의 돈으론 내기를 하지 못니다.\n도박은 가정파탄의 지름길 입니다.\n")
+            print("\n당신의 보유금액으론 내기를 하지 못니다.\n도박은 가정파탄의 지름길 입니다.\n")
             print("잔고가 부족하여 게임을 종료합니다 다른 게임으로 돈을 벌고 오세요^0^")
             members[ID] = passwd, money
             store_members(members)
@@ -37,13 +39,15 @@ def roulettegame(money, ID, passwd):
         store_members(members)
 
 def roulettegame2(money, ID, passwd):
-    bat = int(input("내기로 거실 금액을 입력해주세요 이기면 두배 지면 전부 잃음!"))
+    members = load_members()
+    print("현재 ", money, "원 보유중입니다")
+    bat = int(input("내기로 거실 금액을 입력해주세요 이기면 두배 지면 전부 잃음!\n"))
     if (money>=bat):
         money -= bat
         print(bat,"원이 차감되어 ",money, "원을 갖고 있습니다.")
         roulette_main(money, ID, passwd, bat)
     else:
-        print("당신의 돈으론 내기를 하지 못니다.\n도박은 가정파탄의 지름길 입니다.\n")
+        print("\n당신의 보유금액으론 내기를 하지 못니다.\n도박은 가정파탄의 지름길 입니다.\n")
         print("잔고가 부족하여 게임을 종료합니다 다른 게임으로 돈을 벌고 오세요^0^")
         members[ID] = passwd, money
         store_members(members)
@@ -61,9 +65,6 @@ def rouletteregame(money, ID, passwd):
         members[ID] = passwd, money
         store_members(members)
         print("게임을 종료합니다")
-
-def ask():
-    pass
 
 
 def roulette_main(money, ID, passwd, bat):
@@ -93,6 +94,8 @@ def roulette_main(money, ID, passwd, bat):
             else :
                 break
         print("현재 ",index,"번째 총알을 골랐습니다.")
+        print(real_bullet)
+        print(real_bullet[index-1])
         time.sleep(0.5)
         print("장전완료")
         time.sleep(0.5)
@@ -101,18 +104,21 @@ def roulette_main(money, ID, passwd, bat):
         print("2")
         time.sleep(0.5)
         print("1")
-        del real_bullet[index]
-        if(real_bullet[index] == 1):
+        if(real_bullet[index-1] == 1):
             print("빵!... 당신은 죽었습니다 내기금액을 모두 잃습니다")
             break
         else:
-            print("후.. 빈 총알이군요 한 번 더 뽑으세요.")
+            print("후.. 빈 총알이군요")
             cnt += 1
+            del real_bullet[index-1]
             if (cnt == 3):
                 print("축하합니다 당신은 고난을 모두 견뎌냈어요")
                 print("상금 ", win_bat,"원을 드립니다")
                 money += win_bat
                 break
+            else:
+                print("한번 더 뽑으세요!")
+
     members[ID] = passwd, money
     store_members(members)
     rouletteregame(money, ID, passwd)
