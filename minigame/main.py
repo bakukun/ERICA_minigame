5afc2d5b32b676d14cad799d90d71c273a8f4a2b
Bakukun <h1101201@gmail.com>
2019-12-06 16:45:01 +0900

    게임 구매가 가능하도록, 코드 기반 수정
    기능 추가는 아직 안했지만, 숫자 값을 판단해서 게임실행이 가능하고 구매가능 하도록 할 것임.

--- minigame/main.py
+++ minigame/main.py
@@ -7,7 +7,7 @@ from icecream import *
 import time
 
 def main():
-    ID, money, passwd = login(load_members())
+    ID, money, passwd, icecream_ok, roulette_ok, theif_ok = login(load_members())
     print(ID, "님 안녕하세요!")
     ment = "ERCIA OPEN_SW 미니게임입니다\n"
     for i in ment:
@@ -17,12 +17,12 @@ def main():
     for k in ment2:
         time.sleep(0.3)
         print(k, end='')
-    while (main2(money,ID,passwd) != 0):
+    while (main2(money,ID,passwd, icecream_ok, roulette_ok, theif_ok) != 0):
         money = load(ID)
         time.sleep(0.5)
 
 
-def main2(money, ID, passwd):
+def main2(money, ID, passwd, icecream_ok, roulette_ok, theif_ok):
     if (money >= 0):
         print("현재 ", money, "원을 보유하고 계십니다")
     elif (money < 0):
@@ -34,7 +34,8 @@ def main2(money, ID, passwd):
     print("3. 베스킨라빈스 31")
     print("4. 러시안룰렛")
     print("5. 도둑 잡기")
-    print("6. 나가기")
+    print("6. 게임 구매 상점")
+    print("7. 나가기")
     number = input("플레이 하실 게임의 번호를 선택해주세요\n")
     while (number != "1" and number != "2" and number != "3" and number != "4" and number != "5" and number != "6"):
         number = input("숫자를 정확하게 입력해주세요!\n")
@@ -49,6 +50,8 @@ def main2(money, ID, passwd):
     elif (number == "5"):
         Thief_catch(ID)
     elif (number == "6"):
+        store(money, ID , passwd, icecream_ok, roulette_ok, thief_ok)
+    elif (number == "7"):
         print("================================")
         print("게임을 종료합니다 또 만나요~")
         return 0
--- minigame/new_login.py
+++ minigame/new_login.py
@@ -5,14 +5,14 @@ def login(members):
         ID = input("아이디의 형식이 적절하지 않습니다! 다시 입력해주세요\n")
     trypasswd = input("비밀번호를 입력하세요\n새로운 계정을 만드는 경우라면, 지금 입력하는 문자열이 비밀번호입니다!\n")
     if ID in members:
-        passwd, money = members[ID]
+        passwd, money, icecream_ok, roulette_ok, theif_ok = members[ID]
         if trypasswd == passwd:
-            return ID, money, passwd
+            return ID, money, passwd, icecream_ok, roulette_ok, theif_ok
         else:
             print("비밀번호가 틀립니다. 처음 화면으로 돌아갑니다\n")
             return login(members)
     else:
-        members[ID] = (trypasswd,10000)
+        members[ID] = (trypasswd,10000,0,0,0)
         store_members(members)
         print("새로운 계정을 생성했습니다. 새로운 계정으로 로그인 해주세요\n")
         login(load_members())
@@ -22,8 +22,8 @@ def store_members(members):
     file = open("members.txt","w")
     names = members.keys()
     for name in names:
-        passwd ,money = members[name]
-        data = name + ',' + passwd + ',' + str(money) + '\n'
+        passwd ,money, icecream_ok, roulette_ok, theif_ok = members[name]
+        data = name + ',' + passwd + ',' + str(money) + ',' + str(icecream_ok) + ',' + str(roulette_ok) + ',' + str(theif_ok) + '\n'
         file.write(data)
     file.close()
 
@@ -31,12 +31,15 @@ def load_members():
     file = open("members.txt","r")
     members = {}
     for data in file:
-        name, passwd, money = data.strip('\n').split(',')
-        members[name] = (passwd,int(money))
+        name, passwd, money, icecream_ok, roulette_ok, theif_ok = data.strip('\n').split(',')
+        members[name] = (passwd,int(money), int(icecream_ok), int(roulette_ok), int(theif_ok))
     file.close()
     return members
 
 def load(ID):
     members = load_members()
     money = members[ID][1]
-    return money
+    icecream_ok = members[ID][2]
+    roulette_ok = members[ID][3]
+    theif_ok = members[ID][4]
+    return money,icecream_ok,roulette_ok,theif_ok
