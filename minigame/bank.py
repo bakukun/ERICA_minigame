337138b5ed561b41fa94a5b57c98757c488a9717
Bakukun <h1101201@gmail.com>
2019-12-09 01:14:20 +0900

    돈이 모자랄 때 구제를 받도록 기능 추가함

---
+++ minigame/bank.py
@@ -0,0 +1,20 @@
+from new_login import *
+import time
+
+
+def bank_main(money, ID, passwd, icecream_ok, roulette_ok, theif_ok):
+    members = load_members()
+
+    print("되돌릴 수 없는 빚을 진 여러분들을 위한 긴급충전소입니다.\n보유금액이 10000원 이하인 경우 빚을 탕감해줍니다")
+    time.sleep(1)
+    if (money > 10000):
+        print("구제 대상이 아닙니다. 초기 화면으로 돌아갑니다")
+    else:
+        money = 10000
+        print("구제 대상에 포함됩니다!")
+        time.sleep(1)
+        print("빚 청산과 함께", money, "원을 추가로 충전해 드렸습니다.")
+        print("도박은 가정파탄의 지름길 입니다. 착하게 사세용")
+        time.sleep(0.2)
+    members[ID] = passwd, money, icecream_ok, roulette_ok, theif_ok
+    store_members(members)
